34f648ef86eccdea717149f3b58555ac544bd0da
abdala-elgendy <111305293+abdala-elgendy@users.noreply.github.com>
2024-12-18 07:11:22 +0200

    Update TagService.java

--- src/main/java/com/abdala/demo/service/TagService.java
+++ src/main/java/com/abdala/demo/service/TagService.java
@@ -1,18 +1,18 @@
-//package com.abdala.demo.service;
-//
-//import com.abdala.demo.entity.Tag;
-//import com.abdala.demo.service.dto.TagDTO;
-//
-//import java.util.List;
-//
-//public interface TagService {
-//    Tag createTag(Tag tag);
-//    Tag getTagByName(String name);
-//
-//    TagDTO createTag(TagDTO tagDTO);
-//
-//    List<TagDTO> getAllTags();
-//
-//    TagDTO getTagById(Long id);
-//
-//}
+package com.abdala.demo.service;
+
+import com.abdala.demo.entity.Tag;
+import com.abdala.demo.service.dto.TagDTO;
+
+import java.util.List;
+
+public interface TagService {
+   Tag createTag(Tag tag);
+   Tag getTagByName(String name);
+
+   TagDTO createTag(TagDTO tagDTO);
+
+   List<TagDTO> getAllTags();
+
+   TagDTO getTagById(Long id);
+
+}
