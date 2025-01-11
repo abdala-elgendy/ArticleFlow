95c9be206b2c860d0f529e702d9787b15750ff56
abdullah <elgendyabdala@gmail.com>
2025-01-11 23:53:33 +0200

    update article part

--- src/main/java/com/abdala/demo/DataInitializer.java
+++ src/main/java/com/abdala/demo/DataInitializer.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+ import java.util.Random;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
